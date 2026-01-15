721f8c976ba1e0c03aff40dd1041790622cb1c69
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-15 12:11:57 +0200

    Create lambda_roles.tf

---
+++ iam/lambda_roles.tf
@@ -0,0 +1,24 @@
+
+resource "aws_iam_role" "lambda_role" {
+name = "${var.project_name}-lambda-role"
+
+
+assume_role_policy = jsonencode({
+Version = "2012-10-17"
+Statement = [
+{
+Effect = "Allow"
+Principal = {
+Service = "lambda.amazonaws.com"
+}
+Action = "sts:AssumeRole"
+}
+]
+})
+}
+
+
+resource "aws_iam_role_policy_attachment" "basic_logs" {
+role = aws_iam_role.lambda_role.name
+policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
