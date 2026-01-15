007ecbe7560e1c75b78881b4f3ac6dcef174d63a
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-15 12:18:35 +0200

    update

--- iam/lambda_roles.tf
+++ iam/lambda_roles.tf
@@ -22,3 +22,29 @@ resource "aws_iam_role_policy_attachment" "basic_logs" {
 role = aws_iam_role.lambda_role.name
 policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+
+resource "aws_iam_policy" "dynamo_access" {
+name = "${var.project_name}-dynamo-access"
+
+
+policy = jsonencode({
+Version = "2012-10-17"
+Statement = [
+{
+Effect = "Allow"
+Action = [
+"dynamodb:PutItem",
+"dynamodb:GetItem",
+"dynamodb:Query"
+]
+Resource = aws_dynamodb_table.chat_history.arn
+}
+]
+})
+}
+
+resource "aws_iam_role_policy_attachment" "dynamo_attach" {
+role = aws_iam_role.lambda_role.name
+policy_arn = aws_iam_policy.dynamo_access.arn
+}
