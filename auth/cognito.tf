ebfcafae2e5b39690853f3a5ab189ed68a0b2126
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-15 12:27:40 +0200

    Create cognito.tf

---
+++ auth/cognito.tf
@@ -0,0 +1,42 @@
+
+resource "aws_cognito_user_pool" "this" {
+name = "${var.project_name}-users"
+
+
+auto_verified_attributes = ["email"]
+
+
+password_policy {
+minimum_length = 8
+require_lowercase = true
+require_numbers = true
+require_symbols = true
+require_uppercase = true
+}
+}
+
+
+resource "aws_cognito_user_pool_client" "client" {
+name = "${var.project_name}-client"
+user_pool_id = aws_cognito_user_pool.this.id
+
+generate_secret = false
+
+explicit_auth_flows = [
+"ALLOW_USER_PASSWORD_AUTH",
+"ALLOW_REFRESH_TOKEN_AUTH",
+"ALLOW_USER_SRP_AUTH"
+]
+}
+
+
+resource "aws_cognito_identity_pool" "this" {
+identity_pool_name = "${var.project_name}-identity"
+allow_unauthenticated_identities = false
+
+cognito_identity_providers {
+client_id = aws_cognito_user_pool_client.client.id
+provider_name = aws_cognito_user_pool.this.endpoint
+server_side_token_check = false
+ }
+}
