10aab82d89fee15e5c00c89439ef2c5bab056784
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-15 13:34:41 +0200

    update

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,15 @@
-
 output "vpc_id" {
-value = module.network.vpc_id
+  value = module.network.vpc_id
 }
 
 output "lambda_role_arn" {
-value = module.iam.lambda_role_arn
+  value = module.iam.lambda_role_arn
+}
+
+output "chat_api_endpoint" {
+  value = module.api.graphql_endpoint
+}
+
+output "frontend_url" {
+  value = module.frontend.cloudfront_domain
 }
