b8d494144d01ed1aff06216b3ac4167382f3d322
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-15 13:43:07 +0200

    Update vpc.tf

--- network/vpc.tf
+++ network/vpc.tf
@@ -1,10 +1,8 @@
 
 resource "aws_vpc" "this" {
-cidr_block = var.vpc_cidr
-enable_dns_support = true
-enable_dns_hostnames = true
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
-tags = {
-Name = "${var.project_name}-vpc"
-}
+  tags = { Name = "${var.project_name}-vpc" }
 }
