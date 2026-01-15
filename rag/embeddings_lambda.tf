787680db103d8559ee51a4c4c0eb730ca6c53e1c
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-15 12:21:47 +0200

    Update embeddings_lambda.tf

--- rag/embeddings_lambda.tf
+++ rag/embeddings_lambda.tf
@@ -1,1 +1,18 @@
 
+resource "aws_lambda_function" "embeddings" {
+function_name = "${var.project_name}-embeddings"
+role = module.iam.lambda_role_arn
+handler = "handler.main"
+runtime = "python3.11"
+
+
+filename = "lambda/embeddings.zip"
+
+
+environment {
+variables = {
+MODEL_ID = "amazon.titan-embed-text-v1"
+COLLECTION = aws_opensearchserverless_collection.vector.name
+}
+}
+}
