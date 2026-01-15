a47c1f30a35413c2b80f4c483b46983f30424ed7
Yevgeni Shapiro <23049337+yevgenis1980@users.noreply.github.com>
2026-01-15 13:41:06 +0200

    update

--- iam/bedrock.tf
+++ iam/bedrock.tf
@@ -1,25 +1,16 @@
-
 resource "aws_iam_policy" "bedrock_invoke" {
-name = "${var.project_name}-bedrock-invoke"
-
-
-policy = jsonencode({
-Version = "2012-10-17"
-Statement = [
-{
-Effect = "Allow"
-Action = [
-"bedrock:InvokeModel",
-"bedrock:InvokeModelWithResponseStream"
-]
-Resource = "*"
+  name = "${var.project_name}-bedrock-invoke"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["bedrock:InvokeModel", "bedrock:InvokeModelWithResponseStream"]
+      Resource = "*"
+    }]
+  })
 }
-]
-})
-}
-
 
 resource "aws_iam_role_policy_attachment" "bedrock_attach" {
-role = aws_iam_role.lambda_role.name
-policy_arn = aws_iam_policy.bedrock_invoke.arn
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.bedrock_invoke.arn
 }
